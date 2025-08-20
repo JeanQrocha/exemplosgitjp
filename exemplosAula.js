7635fd242806011cb8f541af81af1496b5d6f53a
Jean Rocha <jean.qrocha96@gmail.com>
2025-08-19 22:29:13 -0300

    exercicio-casa-concluido

---
+++ exemplosAula.js
@@ -0,0 +1,49 @@
+        //PEGANDO UM ELEMENTO PELO ID E MUDANDO A COR COM JS
+//-----------------------------------------------------------------------------------------
+        // const exemploH2 = document.getElementById("exemplo-get");
+        // exemploH2.style.color = 'blue';
+
+//-----------------------------------------------------------------------------------------
+        //ADICIONANDO NOVO ELEMENTO COM O JS
+//-----------------------------------------------------------------------------------------
+        // const novaH1 = document.createElement('h1');
+        // novaH1.textContent = "NOVO ELEMENTO H1 NA TELA";
+        // console.log("NOVO H1 ====>",novaH1);
+        // exemploH2.appendChild(novaH1);
+//-----------------------------------------------------------------------------------------
+
+        //MUDANDO A COR DO HEADER COM O JS
+//-----------------------------------------------------------------------------------------
+        // const novoHeader = document.getElementById("cabeca");
+        // novoHeader.style.backgroundColor = 'red'
+//-----------------------------------------------------------------------------------------
+        //MUDANDO O FOOTER COM O JS
+//-----------------------------------------------------------------------------------------
+        // const novoFooter = document.getElementById("pe");
+        // novoFooter.style.fontSize = "32px"
+     
+//-----------------------------------------------------------------------------------------
+        // quando clicar no botao vai abrir uma barra digitavel
+        // const button = document.getElementById("botao");
+        // button.addEventListener('click', () => {
+        //     prompt("digite um numero")
+        // });
+
+
+
+
+        // const form = document.querySelector("form");
+        // form.addEventListener('submit',(event) => {
+        //     event.preventDefault();
+        //     console.log("batatatattata")
+        // });
+        
+        // //-------flodar alerta na tela quando passa o mouse -----
+        // const cabeca = document.getElementById("cabeca");
+        // cabeca.addEventListener('mouseover', () => {
+        //     alert('sai fora');
+        // });
+        //CAPITURA AS TECLAS QUE FORAM CLICADAS DENTRO DA PAGINA
+        // document.addEventListener('keydown', (event) => {
+        //     console.log(`tecla clicado ${event.key}`)
+        // });
